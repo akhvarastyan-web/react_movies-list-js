191f5b738e0beaecc2d30ff702477572e8f2f02b
akhvarastyan-web <a.khvarastyan@gmail.com>
2026-01-02 18:52:14 +0200

    second

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MovieList } from './components/MovieList/MovieList';
 
-
 export const App = () => (
   <div className="page">
     <div className="page-content">
-     <MovieList movies={moviesFromServer}/>
+      <MovieList movies={moviesFromServer} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
